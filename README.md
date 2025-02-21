54de27325693eecaea7f750cba9023ceda5d2955
DumbNox <kevinmarcano0921@gmail.com>
2025-02-21 05:55:19 -0400

    ffmpeg-bash-god_v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # ffmpeg-bash-god
 Hello! ffmpeg-bash-god is a collection of scripts to record your screen (this is mostly not useful for people with a good PC, however, there are people in the world who have a somewhat limited PC). So I'm bringing you something frankly easy and very lightweight.
+
+## Installation
+
+The only thing you have to do is clone the repository:
+
+```bash
+```
+```
+```git clone git@github.com:DumbNoxx/ffmpeg-bash-god.git`
+```
+Next, you just need to navigate to the folder and run this:
+
+```
+```
+```./ffmcap.sh```
+```
+```
+
+And you’re all set!!
+
+## Usage
+Once you have executed `./ffmcap.sh,` you can simply use the command ffmcap, and it will start the screen recorder. All you need to do is follow the instructions, and you’ll be ready to record your screen!!!
+
+## Warnings
+In some cases, `/ffmcap.sh` might not execute properly, so you might have to do it manually. What do I mean?
+
+You may need to create the alias manually and give execution permissions. If you have no idea how to do it, don't worry, I'll help you with that.
+
+## Manual Installation
+Fist, clone the repository:
+
+```
+```git clone git@github.com:DumbNoxx/ffmpeg-bash-god.git```
+Next, navigate to the folder where the .sh file is located. So far, everything is the same as at the beginning, but here things change.
+
+When you reach the destination folder, instead of using `./ffmcap.sh`, we will do it manually:
+
+```
+```echo alias ffmcap="absolute_path_to_your_file/ffmcap.sh" >> ~/.bashrc```
+After doing that, you will have the alias in your commands, but it's not activated yet. You need to activate it.
+
+```
+```
+```chmod +x ffmcap.sh```
+```
+```
+
+First, give execution permissions to the file. After doing that...
+
+```
+```source ~/.bashrc```
+Or simply close and open the terminal again to reload the commands.
+
+And you will be ready to execute the command ffmcap, allowing you to record your screen with ease.
+
+Suggestions for Possible Errors
+It is possible that when executing `./ffmcap.sh`, the command does not activate directly. My recommendation would be to use chmod and activate the command using source or by restarting the terminal to rule out any doubts about a failure.
+
+## Final
+Honestly, this is a bit silly, but I thought I would do it for people who are just starting out and may not have a powerful PC to record their screen, or who want to save resources. This is truly useful, and of course, I will continue to improve this tool. I aim to make it compatible with Mac, and perhaps I will create a simplified version for Windows.
+
+I hope you liked it, and please give the repo a star if it helped you. Cheers!
--- ffmcap.sh
+++ ffmcap.sh
@@ -1,62 +1,95 @@
 #!/bin/bash
+# Ignoraren esto
+SCRIPT_PATH="$(pwd)/ffmcap.sh"
 
+# Asegurarse de que el script sea ejecutable
+chmod +x "$SCRIPT_PATH"
+
+if ! grep -q "alias ffmcap=" ~/.bashrc; then
+  echo "Agregando alias a .bashrc..."
+  echo "alias ffmcap='$SCRIPT_PATH'" >>~/.bashrc
+  echo "Alias añadido."
+else
+  echo "El alias 'ffmcap' ya está configurado en .bashrc."
+fi
+
+source ~/.bashrc
+
+clear
+
+echo "====================================="
+echo "           BIENVENIDOS A            "
+echo "          ffmpeg-bash-god           "
+echo "====================================="
+
+echo ""
+echo "¡Disfruta grabando tu pantalla! Espero te sea util, y recuerda apoyarme en mi"
+echo "github: https://github.com/DumbNoxx"
+echo ""
+
+echo "Cargando..."
+for i in {1..3}; do
+  sleep 0.5
+  echo -n "."
+done
+echo ""
 # Función para verificar si ffmpeg está instalado
 check_ffmpeg() {
-    if command -v ffmpeg &> /dev/null; then
-        echo "Ya tienes ffmpeg instalado que bien!!."
-        return 0
-    else
-        echo "ffmpeg no está instalado."
-	echo " "
-	read -p "Quieres instalarlo (y/s/yes/YES/n/no/NO)" respuesta
-	respuesta=${respuesta,,}
-	if [[ "$respuesta" == *"y"* ]] || [[ "$respuesta" == *"s"* ]] || [[ "$respuesta" == *"si"* ]] || [[ "$respuesta" == *"yes"* ]]; then
-		echo " "
-		echo "Perfecto, vamos a ello"
-		echo " "
-        	return 1
-	elif [[ "$respuesta" == *"n"* ]] || [[ "$respuesta" == *"no"* ]]; then
-		echo " "
-		echo "Entiendo, puedes descargarlo por tu cuenta si deseas, si no, no podras ejecutar el programa"
-		echo " "
-		exit 0
-	fi
+  if command -v ffmpeg &>/dev/null; then
+    echo "Ya tienes ffmpeg instalado que bien!!."
+    return 0
+  else
+    echo "ffmpeg no está instalado."
+    echo " "
+    read -p "Quieres instalarlo (y/s/yes/YES/n/no/NO)" respuesta
+    respuesta=${respuesta,,}
+    if [[ "$respuesta" == *"y"* ]] || [[ "$respuesta" == *"s"* ]] || [[ "$respuesta" == *"si"* ]] || [[ "$respuesta" == *"yes"* ]]; then
+      echo " "
+      echo "Perfecto, vamos a ello"
+      echo " "
+      return 1
+    elif [[ "$respuesta" == *"n"* ]] || [[ "$respuesta" == *"no"* ]]; then
+      echo " "
+      echo "Entiendo, puedes descargarlo por tu cuenta si deseas, si no, no podras ejecutar el programa"
+      echo " "
+      exit 0
     fi
+  fi
 }
 
 # Función para instalar ffmpeg
 install_ffmpeg() {
-    echo "Instalando ffmpeg..."
+  echo "Instalando ffmpeg..."
 
-    # Detección del gestor de paquetes
-    if [ -x "$(command -v apt)" ]; then
-        # Para distribuciones basadas en Debian/Ubuntu
-        sudo apt update
-        sudo apt install -y ffmpeg
-    elif [ -x "$(command -v pacman)" ]; then
-        # Para Arch Linux
-        sudo pacman -Syu --noconfirm ffmpeg
-    elif [ -x "$(command -v dnf)" ]; then
-        # Para Fedora
-        sudo dnf install -y ffmpeg
-    elif [ -x "$(command -v yum)" ]; then
-        # Para CentOS/RHEL
-        sudo yum install -y ffmpeg
-    elif [ -x "$(command -v zypper)" ]; then
-        # Para openSUSE
-        sudo zypper install -y ffmpeg
-    else
-        echo "No se pudo determinar el gestor de paquetes. Por favor, instala ffmpeg manualmente."
-        exit 1
-    fi
-    echo " "
-    echo "ffmpeg ha sido instalado."
-    echo " "
+  # Detección del gestor de paquetes
+  if [ -x "$(command -v apt)" ]; then
+    # Para distribuciones basadas en Debian/Ubuntu
+    sudo apt update
+    sudo apt install -y ffmpeg
+  elif [ -x "$(command -v pacman)" ]; then
+    # Para Arch Linux
+    sudo pacman -Syu --noconfirm ffmpeg
+  elif [ -x "$(command -v dnf)" ]; then
+    # Para Fedora
+    sudo dnf install -y ffmpeg
+  elif [ -x "$(command -v yum)" ]; then
+    # Para CentOS/RHEL
+    sudo yum install -y ffmpeg
+  elif [ -x "$(command -v zypper)" ]; then
+    # Para openSUSE
+    sudo zypper install -y ffmpeg
+  else
+    echo "No se pudo determinar el gestor de paquetes. Por favor, instala ffmpeg manualmente."
+    exit 1
+  fi
+  echo " "
+  echo "ffmpeg ha sido instalado."
+  echo " "
 }
 
 # Verificar si ffmpeg está instalado
 if ! check_ffmpeg; then
-    install_ffmpeg
+  install_ffmpeg
 fi
 # Pedir al usuario el tamaño del video
 dimensions=$(xdpyinfo | grep dimensions)
@@ -92,13 +125,13 @@ echo " "
 read -p "Ingrese el nombre del archivo con la extension (ejemplo: output(sin la extensión)):" output_name
 
 # Comando completo de ffmpeg
-ffmpeg -f x11grab -video_size "$video_size" -framerate "$framerate" -i "$video_input" -f pulse -i "$audio_input" -map 0:v -map 1:a -c:v libx264 -preset ultrafast -crf 18 -c:a aac -b:a 192k "rendered_$output_name.mp4" 2>&1 | \
-while IFS= read -r line; do 
-if [[ "$line" == *"frame="* || "$line" == *"Lsize="* || "$line" == *"time="* ]]; then 
-	echo " "
-	echo "$line" 
-fi 
-done &
+ffmpeg -f x11grab -video_size "$video_size" -framerate "$framerate" -i "$video_input" -f pulse -i "$audio_input" -map 0:v -map 1:a -c:v libx264 -preset ultrafast -crf 18 -c:a aac -b:a 192k "rendered_$output_name.mp4" 2>&1 |
+  while IFS= read -r line; do
+    if [[ "$line" == *"frame="* || "$line" == *"Lsize="* || "$line" == *"time="* ]]; then
+      echo " "
+      echo "$line"
+    fi
+  done &
 
 # Mostrar el mensaje de grabación
 echo " "
@@ -107,45 +140,45 @@ echo " "
 echo "Grabando..."
 
 # Esperar a que ffmpeg termine
-wait 
+wait
 # Manejo de errores
 if [ $? -eq 0 ]; then
-	echo " "
-	echo "Grabación completada con éxito."
-	# Tomando el nombre de la variable por cualquier cosa que pueda suceder
-	out_video="$output_name"
-	# Ejecutando el renderizado
-	ffmpeg -i "rendered_$output_name.mp4" -vcodec libx264 -af "anlmdn" -acodec aac "$out_video.mp4" 2>&1 | \
-	# Filtrando texto
-	while IFS= read -r line; do
-	if [[ "$line" == *"frame="* ]]; then
-		echo " "
-		echo "$line"
-	fi
-	done &
-	echo " "
-	echo "Renderizando..."
-	# Esperar que el programa termine
-	wait
-	# Manejo de errores del renderizado
-	if [ $? -eq 0 ]; then
-		echo " "
-		echo "Renderizado con Exito..."
-		echo " "
-		echo "Exportanto..."
-		echo " "
-		# Eliminando el video anterior de la grabacion
-		rm "rendered_$output_name.mp4"
-	else
-		echo " "
-		echo "Hubo un error en el renderizado"
-		echo " "
-	fi
-	echo " "
-	echo "Saliendo..."
-	echo " "
+  echo " "
+  echo "Grabación completada con éxito."
+  # Tomando el nombre de la variable por cualquier cosa que pueda suceder
+  out_video="$output_name"
+  # Ejecutando el renderizado
+  ffmpeg -i "rendered_$output_name.mp4" -vcodec libx264 -af "anlmdn" -acodec aac "$out_video.mp4" 2>&1 |
+    # Filtrando texto
+    while IFS= read -r line; do
+      if [[ "$line" == *"frame="* ]]; then
+        echo " "
+        echo "$line"
+      fi
+    done &
+  echo " "
+  echo "Renderizando..."
+  # Esperar que el programa termine
+  wait
+  # Manejo de errores del renderizado
+  if [ $? -eq 0 ]; then
+    echo " "
+    echo "Renderizado con Exito..."
+    echo " "
+    echo "Exportanto..."
+    echo " "
+    # Eliminando el video anterior de la grabacion
+    rm "rendered_$output_name.mp4"
+  else
+    echo " "
+    echo "Hubo un error en el renderizado"
+    echo " "
+  fi
+  echo " "
+  echo "Saliendo..."
+  echo " "
 else
-	echo " "
-	echo "Hubo un error al grabar."
-	echo " "
+  echo " "
+  echo "Hubo un error al grabar."
+  echo " "
 fi
